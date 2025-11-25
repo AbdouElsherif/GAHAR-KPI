fc4f8ab93a782e4e1549edbf8516522ed830e14d
Sara <sara@gahar.gov.sa>
2025-11-25 18:56:29 +0200

    changed minimum pagination items to 12

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -180,7 +180,7 @@ export default function Pagination({
                         fontSize: '0.9rem'
                     }}
                 >
-                    <option value={10}>10</option>
+                    <option value={12}>12</option>
                     <option value={25}>25</option>
                     <option value={50}>50</option>
                     <option value={100}>100</option>
